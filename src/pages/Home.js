bcb261b06a20a11fd794c299573ec56fff827593
unknown <karanrana3095@gmail.com>
2022-07-03 11:38:55 +0530

    created input

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
-const Home = () => {
+const Home=()=>
+{
+  const [Input,setInput]=useState('');
+  const onInputChange=(ev)=>{
+    setInput(ev.target.value);
+      
+  }
+  const onSearch=()=>{
+   
+    fetch(`https://api.tvmaze.com/search/shows?q=
+    =${Input}`).then(
+      r=>r.json()).then(result=>{
+        console.log(result);
+      })
+  };
+
+  const onKeyDown=(ev)=>{
+    if(ev.keyCode==13)
+    {
+      onSearch()
+    }
+    
+  };
+  
+
   return (
-    <MainPageLayout>Home</MainPageLayout>
+    <MainPageLayout>
+      <input type="text" onChange={onInputChange}
+      onKeyDown={onKeyDown}
+      value={Input}/>
+      <button type="button" onClick={onSearch}
+      >Search</button>
+    </MainPageLayout>
   )
-}
+};
 
 export default Home
