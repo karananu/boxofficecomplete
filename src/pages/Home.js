b8fe7056965ff20a8e4fdf8494d5bfb2259448a7
unknown <karanrana3095@gmail.com>
2022-07-04 23:52:29 +0530

    styled the entire app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,28 @@
-import React, { useState, useEffect } from "react";
-import ActorGrid from "../components/actor/ActorGrid";
+import React, { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
-import ShowGrid from "../components/show/ShowGrid";
 import { apiGet } from "../misc/config";
+import ShowGrid from "../components/show/ShowGrid";
+import ActorGrid from "../components/actor/ActorGrid";
 import { useLastQuery } from "../misc/custom-hooks";
+import {
+  SearchInput,
+  RadioInputsWrapper,
+  SearchButtonWrapper,
+} from "./Home.styled";
+import CustomRadio from "../components/CustomRadio";
+
 const Home = () => {
-  const [Input, setInput] = useLastQuery();
+  const [input, setInput] = useLastQuery();
   const [results, setResults] = useState(null);
-  const [searchOption, SetsearchOption] = useState("shows");
+  const [searchOption, setSearchOption] = useState("shows");
 
   const isShowsSearch = searchOption === "shows";
-
-  useEffect(() => {
-    console.log("use effect run");
-    return () => {
-      console.log("exit");
-    };
-  }, []);
-
   const onSearch = () => {
-    apiGet(`/search/${searchOption}?q=
-    =${Input}`).then((result) => {
+    apiGet(`/search/${searchOption}?q=${input}`).then((result) => {
       setResults(result);
     });
   };
+
   const onInputChange = (ev) => {
     setInput(ev.target.value);
   };
@@ -33,13 +32,16 @@ const Home = () => {
       onSearch();
     }
   };
+
   const onRadioChange = (ev) => {
-    SetsearchOption(ev.target.value);
+    setSearchOption(ev.target.value);
   };
+
   const renderResults = () => {
     if (results && results.length === 0) {
-      return <div>No Result</div>;
+      return <div>No results</div>;
     }
+
     if (results && results.length > 0) {
       return results[0].show ? (
         <ShowGrid data={results} />
@@ -53,39 +55,41 @@ const Home = () => {
 
   return (
     <MainPageLayout>
-      <input
+      <SearchInput
         type="text"
-        placeholder="Search for Something"
+        placeholder="Search for something"
         onChange={onInputChange}
         onKeyDown={onKeyDown}
-        value={Input}
+        value={input}
       />
 
-      <div>
-        <label htmlFor="shows-search">
-          Shows
-          <input
+      <RadioInputsWrapper>
+        <div>
+          <CustomRadio
+            label="Shows"
             id="shows-search"
-            type="radio"
-            value="show"
+            value="shows"
             checked={isShowsSearch}
             onChange={onRadioChange}
           />
-        </label>
-        <label htmlFor="actors-search">
-          Actors
-          <input
+        </div>
+
+        <div>
+          <CustomRadio
+            label="Actors"
             id="actors-search"
-            type="radio"
-            checked={!isShowsSearch}
             value="people"
+            checked={!isShowsSearch}
             onChange={onRadioChange}
           />
-        </label>
-      </div>
-      <button type="button" onClick={onSearch}>
-        Search
-      </button>
+        </div>
+      </RadioInputsWrapper>
+
+      <SearchButtonWrapper>
+        <button type="button" onClick={onSearch}>
+          Search
+        </button>
+      </SearchButtonWrapper>
       {renderResults()}
     </MainPageLayout>
   );
