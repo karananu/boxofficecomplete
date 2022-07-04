6e15f867f9c70e325cb785fa3191afda11b63eb4
unknown <karanrana3095@gmail.com>
2022-07-04 14:13:21 +0530

    aded show

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-
-import {Switch,Route} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Anu from "./pages/Anu";
 import Home from "./pages/Home";
+import Show from "./pages/Show";
 function App() {
   return (
-  
     <Switch>
       <Route exact path="/">
-        <Home/>
+        <Home />
       </Route>
       <Route exact path="/anu">
-        <Anu/>
+        <Anu />
+      </Route>
+      <Route exact path="/show/:id">
+        <Show />
       </Route>
       <Route>
-       <div>
-        Not Found
-       </div>
+        <div>Not Found</div>
       </Route>
     </Switch>
-   
   );
 }
 
